ee796baabfca04edc83f742790fb0f9ea7c64e5f
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-06-18 00:16:17 +0100

    made new changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
 import Resume from "../assets/images/resume.pdf";
+import Jay from "../assets/images/Jay2.png";
 const Home = () => {
   return (
     <div id="home">
-      <div className="homeFirst">
+      <div className="homeLink">
         <div className="HomeLink">
           <a href="tel:+2348104434732" target="_blank">
             +2348104434732
@@ -12,19 +13,31 @@ const Home = () => {
           </a>
         </div>
         <div>
-          <h2>Hello, My name is</h2>
-          <h1>Nkemdili Jane</h1>
-          <p>
-            I design and develop services for customers of all sizes,
-            specializing in creating stylish, modern websites, web services and
-            online stores.
-          </p>
-          <a href={Resume} download className="download-button">
-            Download Resume
-          </a>
+          <button className="oran">EN</button>
+          <button>FN</button>
         </div>
       </div>
-      <div className="homeSecond"></div>
+      <div className="home">
+        <div className="homeFirst">
+          <div>
+            <h2>Hello, My name is</h2>
+            <h1>Nkemdili Jane</h1>
+            <p>
+              I design and develop services for customers of all sizes,
+              specializing in creating stylish, modern websites, web services
+              and online stores.
+            </p>
+            <a href={Resume} download className="download-button">
+              Download Resume
+            </a>
+          </div>
+        </div>
+        <div className="homeSecond">
+          <div className="img">
+            <img src={Jay} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
