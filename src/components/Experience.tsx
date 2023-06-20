a450da7a31a6f1ddb37f31b9f960865541bc03b9
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-06-21 00:23:27 +0100

    finished the experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,6 +2,63 @@ const Experience = () => {
   return (
     <div id="exp">
       <h1 className="title">Experience</h1>
+      <div className="expCard">
+        <div className="bioCard1">
+          <div className="biotxt">
+            <h3>Frontend Intern </h3>
+            <p>Remote</p>
+            <p>Dec 2022 - present</p>
+          </div>
+          <span>Partime</span>
+        </div>
+        <div className="bioCard2">
+          <h3>Genesys Techub</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            officia aperiam reprehenderit et dolorum iste ipsam, ullam aliquam.
+            Dolorum libero at delectus? officia aperiam reprehenderit et dolorum
+            iste ipsam, ullam aliquam. Dolorum libero at delectus?
+          </p>
+        </div>
+      </div>
+      <div className="expCard">
+        <div className="bioCard1">
+          <div className="biotxt">
+            <h3>Tutor </h3>
+            <p>Remote</p>
+            <p>Feb 2023 - June 2023</p>
+          </div>
+          <span>Partime</span>
+        </div>
+        <div className="bioCard2">
+          <h3>HerTechTrail</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            officia aperiam reprehenderit et dolorum iste ipsam, ullam aliquam.
+            Dolorum libero at delectus? officia aperiam reprehenderit et dolorum
+            iste ipsam, ullam aliquam. Dolorum libero at delectus?
+          </p>
+        </div>
+      </div>
+      <div className="expCard">
+        <div className="bioCard1">
+          <div className="biotxt">
+            <h3>Frontend Developer </h3>
+            <p>Remote</p>
+            <p>Sept 2022 - January 2023</p>
+          </div>
+          <span>Contract</span>
+        </div>
+        <div className="bioCard2">
+          <h3>Echub</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            officia aperiam reprehenderit et dolorum iste ipsam, ullam aliquam.
+            Dolorum libero at delectus? officia aperiam reprehenderit et dolorum
+            iste ipsam, ullam aliquam. Dolorum libero at delectus?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
