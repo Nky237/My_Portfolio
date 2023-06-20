68cd27ef427dc091d35dc64901625ee72c8cd192
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-06-20 23:49:31 +0100

    i did the what i do section

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,9 +1,50 @@
+import { MdPhonelink, MdOutlineDesktopWindows } from "react-icons/md";
+import { GiTeacher } from "react-icons/gi";
+
 const Job = () => {
   return (
-    <div id="port">
-      <h1 className="title">Portfolio</h1>
+    <div id="bio">
+      <h1 className="title">What I do?</h1>
+      <div className="title-cards">
+        <div className="cardbio">
+          <div className="rad">
+            <MdPhonelink className="icon" />
+          </div>
+          <div className="biotext">
+            <h3>Web Design</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+              commodi saepe vitae repellat iusto quia?
+            </p>
+          </div>
+        </div>
+        <div className="cardbio">
+          <div className="rad">
+            <MdOutlineDesktopWindows className="icon" />
+          </div>
+          <div className="biotext">
+            <h3>Technical Writing</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+              commodi saepe vitae repellat iusto quia?
+            </p>
+          </div>
+        </div>
+        <div className="cardbio">
+          <div className="rad">
+            <GiTeacher className="icon" />
+          </div>
+          <div className="biotext">
+            <h3>Tutoring</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+              commodi saepe vitae repellat iusto quia?
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Job
+export default Job;
