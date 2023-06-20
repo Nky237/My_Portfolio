5f13ee824f1c235daf680ce92c87fe398b3106b4
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-06-21 00:58:50 +0100

    finished the education section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,8 +2,47 @@ const Skills = () => {
   return (
     <div id="skill">
       <h1 className="title">Education and skills</h1>
+      <div className="edu">
+        <div className="edu1">
+          <div className="edu0">
+            <p>2016 - 2022</p>
+            <h3>B.eng in Electronic Engineering</h3>
+            <p>University Of Nigeria, Nsukka</p>
+          </div>
+          <div className="edu0">
+            <p>March 2022 - July 2022</p>
+            <h3>Website Development</h3>
+            <p>Kodecamp, Remote</p>
+          </div>
+          <div className="edu0">
+            <p>Feb 2021 - Oct 2021</p>
+            <h3>Website Development</h3>
+            <p>Applied Technology, Enugu</p>
+          </div>
+        </div>
+        <div className="edu2">
+          <div>
+            <h3>HTNL5</h3>
+          </div>
+          <div>
+            <h3> CSS || Bootstrap || Tailwind || Styled Component</h3>
+          </div>
+          <div>
+            <h3>ReactJS</h3>
+          </div>
+          <div>
+            <h3>NextJS</h3>
+          </div>
+          <div>
+            <h3>TypeScript</h3>
+          </div>
+          <div>
+            <h3>Python</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Skills
+export default Skills;
