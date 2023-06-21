159595757e2d5998984fbe6c494dc3942f745e74
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-06-21 02:06:35 +0100

    did the form section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,51 @@
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { SiGmail } from "react-icons/si";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
 const Contact = () => {
   return (
     <div id="contact">
-      <h1 className="title">Contact Me</h1>
+      {/* <h1 className="title" style={{ textAlign: "center" }}>Contact Me</h1> */}
+      <div className="contact">
+        <div className="contact1">
+          <h3>What’s your story? Get in touch</h3>
+          <p>
+            Always available for freelancing if the right project comes along,
+            Feel free to contact me.
+          </p>
+          <div className="conIcon">
+            <div>
+              <HiOutlineLocationMarker />
+              <p>Gbagada, Lagos State</p>
+            </div>
+            <div>
+              <SiGmail />
+              <p>ezikenkemdilijane@gmail.com</p>
+            </div>
+            <div>
+              <BsFillTelephoneOutboundFill />
+              <p>+2349157364210</p>
+            </div>
+          </div>
+        </div>
+        <div className="contact2">
+          <h2>Say Something</h2>
+          <form action="">
+            <div className="Con">
+              <input type="text" placeholder="full name" />
+              <input type="email" placeholder="Email" />
+            </div>
+            <div>
+              <input type="text" placeholder="Subject" />
+            </div>
+            <div>
+              <textarea name="" id="" placeholder="Type comment"></textarea>
+            </div>
+            <button>Send Message</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
--- src/components/Foot.tsx
+++ src/components/Foot.tsx
@@ -1,7 +1,21 @@
+import { ImFacebook, ImLinkedin2 } from "react-icons/im";
+import { BsTwitter, BsGithub } from "react-icons/bs";
+import { AiOutlineInstagram } from "react-icons/ai";
 const Foot = () => {
   return (
-    <div >Foot</div>
-  )
-}
+    <div id="foot">
+      <div className="foot">
+        <div className="footIcon">
+          <ImFacebook className="footIcon1" />
+          <ImLinkedin2 className="footIcon1" />
+          <BsTwitter className="footIcon1" />
+          <BsGithub className="footIcon1" />
+          <AiOutlineInstagram className="footIcon1" />
+        </div>
+        <div><p>© 2023 copyright <span>Jhenna</span> all right reserved </p></div>
+      </div>
+    </div>
+  );
+};
 
-export default Foot
+export default Foot;
