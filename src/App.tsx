6f90cf866197029e78f6d41bdfc736d87c060ca0
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-07-20 12:09:54 +0100

    added an extra loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
 import Nav from "./Layout/Nav";
 import Page from "./Page";
